d72be1e50521bfd8b5cfbf5cd5703eac9449bf92
Diwakar <diwakar.singh@neosoftmail.com>
2023-01-05 10:26:08 +0530

    dynamic validation

--- app/models/admin/question.rb
+++ app/models/admin/question.rb
@@ -10,5 +10,19 @@ module Admin
     belongs_to :subject
 
     validates_presence_of :inputType, :title
+    validate :options_and_answer
+
+    private
+
+    def options_and_answer
+      if inputType == 'true/false' && %w[true false].exclude?(answer.downcase)
+        errors.add(:answer, ' should be either true or false')
+      elsif (inputType == 'multipleChoice' || inputType == 'list') && (answer.blank? || option1.blank? || option2.blank? || option3.blank? || option4.blank?)
+        errors.add(:all, 'field should be fill')
+      elsif (inputType == 'multipleChoice' || inputType == 'list') && [option1, option2, option3,
+                                                                       option4].exclude?(answer)
+        errors.add(:answer, 'must be one of option.')
+      end
+    end
   end
 end
